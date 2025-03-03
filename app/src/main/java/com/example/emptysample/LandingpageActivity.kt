987b8f66f2980219b0cbbb6816bdee70455b9778
spinachpack <migirada14@gmail.com>
2025-03-03 23:42:06 +0800

    Add Files

--- app/src/main/java/com/example/emptysample/LandingpageActivity.kt
+++ app/src/main/java/com/example/emptysample/LandingpageActivity.kt
@@ -1,20 +1,11 @@
 package com.example.emptysample
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class LandingpageActivity : AppCompatActivity() {
+class LandingpageActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_landingpage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
